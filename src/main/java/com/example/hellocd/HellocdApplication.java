e4af9ed37968f275bb57b52bcf7771aa95aff9f0
root <root@ci.europe-west10-a.c.ci-cd-452210.internal>
2025-03-05 13:11:34 +0000

    adding last changes

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "continuous delivery!";
 	}
 
 	public static void main(String[] args) {
